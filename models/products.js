411a35312ba18cd780ded880dc0ee82f39015c54
brittanyleonard <Belah615@gmail.com>
2020-12-24 16:44:55 -0600

    routes set up

---
+++ models/products.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    img: {type: String, required: false},
+    description: {type: String, required: false},
+    price: {type: Number, required: true},
+    quantity: {type: Number, required: true}
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
+
+
+
+//////// SCHEMA FORMAT TO BE USED ///////
+// name: String,
+//   description: String,
+//   img: String,
+//   price: Number,
+//   quantity: Number
